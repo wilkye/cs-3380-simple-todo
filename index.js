a8e67645338fa0d2bf0e7439f0ec5b6d7337e9bd
wilkye <wilkylegreer@gmail.com>
2026-01-14 13:59:02 -0700

    Basic task creation through objects and storing in an array completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("test");
+const jsonData = "todos.json";
+let tasks = [];
+
+function Task(text) {
+    this.text = text;
+    this.completed = false;
+}
+
+function createTask(text) {
+    const task1 = new Task(text);
+    tasks.push(task1);
+    console.log("Task Created.");
+    console.log(tasks);
+}
+
+createTask("test");
+createTask("better test");
